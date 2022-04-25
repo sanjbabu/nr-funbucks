4dace47538fbff50afd7c3300b4499ff5dffc004
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2022-04-25 10:20:14 -0700

    feat: decrease filter, input and parser limits

--- src/constants/limits.ts
+++ src/constants/limits.ts
@@ -1,14 +1,14 @@
 /**
  * The number of filters to limit the configuration to.
  */
-export const FB_FILTER_LIMIT = 200;
+export const FB_FILTER_LIMIT = 190;
 
 /**
  * The number of inputs to limit the configuration to.
  */
-export const FB_INPUT_LIMIT = 200;
+export const FB_INPUT_LIMIT = 190;
 
 /**
  * The number of parsers to limit the configuration to.
  */
-export const FB_PARSER_LIMIT = 200;
+export const FB_PARSER_LIMIT = 190;
