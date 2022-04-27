f172915d980175f97abedd18a55a23032a4f1f1b
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2022-04-27 11:09:56 -0700

    feat: reduce limits to 180

--- src/constants/limits.ts
+++ src/constants/limits.ts
@@ -1,14 +1,14 @@
 /**
  * The number of filters to limit the configuration to.
  */
-export const FB_FILTER_LIMIT = 190;
+export const FB_FILTER_LIMIT = 180;
 
 /**
  * The number of inputs to limit the configuration to.
  */
-export const FB_INPUT_LIMIT = 190;
+export const FB_INPUT_LIMIT = 180;
 
 /**
  * The number of parsers to limit the configuration to.
  */
-export const FB_PARSER_LIMIT = 190;
+export const FB_PARSER_LIMIT = 180;
